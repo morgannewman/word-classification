e0bc94b9a87ef4844e2c3ed9d4fc186a0b6fa011
Morgan Newman <mpnewman95@gmail.com>
2019-07-31 23:16:26 -0700

    Finishes typing words

--- word_types/main.py
+++ word_types/main.py
@@ -3,59 +3,50 @@ import json
 with open("words_common.json", "r") as f:
     dictionary = json.load(f)
 
-nouns_plural = []
-nouns_singular = []
-adjectives = []
-adverbs = []
-superlatives = []
-verbs_past = []
-verbs_present = []
-verbs_progressive = []
-verbs_transitive = []
+dicts = {
+    "noun_plural": [],
+    "noun_singular": [],
+    "adjective": [],
+    "adverb": [],
+    "superlative": [],
+    "verb_past": [],
+    "verb_present": [],
+    "verb_progressive": [],
+}
 
 for word, parts in dictionary.items():
     for part in parts:
         if part == "other":
             continue
+        if part == "verb:transitive":
+            continue
 
         if part == "noun:plural":
-            nouns_plural.append(word)
+            dicts["noun_plural"].append(word)
             continue
         if part == "noun:singular":
-            nouns_singular.append(word)
+            dicts["noun_singular"].append(word)
             continue
         if part == "adjective":
-            adjectives.append(word)
+            dicts["adjective"].append(word)
             continue
         if part == "adverb":
-            adverbs.append(word)
+            dicts["adverb"].append(word)
             continue
         if part == "superlative":
-            superlatives.append(word)
+            dicts["superlative"].append(word)
             continue
         if part == "verb:past":
-            verbs_past.append(word)
+            dicts["verb_past"].append(word)
             continue
         if part == "verb:present":
-            verbs_present.append(word)
+            dicts["verb_present"].append(word)
             continue
         if part == "verb:progressive":
-            verbs_progressive.append(word)
+            dicts["verb_progressive"].append(word)
             continue
-        if part == "verb:transitive":
-            verbs_transitive.append(word)
 
 
-count = (
-    len(nouns_plural)
-    + len(nouns_singular)
-    + len(adjectives)
-    + len(adverbs)
-    + len(superlatives)
-    + len(verbs_past)
-    + len(verbs_present)
-    + len(verbs_progressive)
-    + len(verbs_transitive)
-)
-
-print(count)
+for name, words in dicts.items():
+    with open("word_types/{}.json".format(name), "w") as f:
+        json.dump(words, f, indent=2)
